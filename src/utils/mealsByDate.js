74d90eb979bc9efe01a1ab8da98048bd11908071
biyunzhu <zhuby85@gmail.com>
2024-03-27 13:43:31 -0600

    Add meal type as placeholder to refactor mealsByDate

--- src/utils/mealsByDate.js
+++ src/utils/mealsByDate.js
@@ -1,10 +1,16 @@
-const mealsByDate = {};
-meals.forEach((meal) => {
-  const dateKey = meal.date;
+function mealsByDate(meals) {
+  const mealsByDate = {};
+  meals.forEach((meal) => {
+    const dateKey = meal.date;
 
-  if (!mealsByDate[dateKey]) {
-    mealsByDate[dateKey] = [];
-  }
+    if (!mealsByDate[dateKey]) {
+      mealsByDate[dateKey] = [];
+    }
 
-  mealsByDate[dateKey].push(meal);
-});
+    mealsByDate[dateKey].push(meal);
+
+    return mealsByDate;
+  });
+}
+
+export default mealsByDate;
