aa0f7fb115ecf3f12a9d70b73fba7d8430ca8099
biyunzhu <zhuby85@gmail.com>
2024-03-27 18:25:49 -0600

    Add recipe name and recipe uuid in formated meal data

--- src/components/MealList2/MealList2.js
+++ src/components/MealList2/MealList2.js
@@ -8,71 +8,6 @@ import mealsDataFormater from "../../utils/mealsDataFormator";
 
 function MealList2({ meals }) {
   console.log(meals);
-  // Group meals by date
-  // const mealsByDate = {};
-
-  // meals.forEach((meal) => {
-  //   const dateKey = meal.date;
-
-  //   if (!mealsByDate[dateKey]) {
-  //     mealsByDate[dateKey] = [];
-  //   }
-
-  //   mealsByDate[dateKey].push(meal);
-  // });
-
-  // const datesForThisWeek = [
-  //   "2024-03-20",
-  //   "2024-03-21",
-  //   "2024-07-03",
-  //   "2024-08-12",
-  //   "2024-09-25",
-  // ];
-
-  // // Initialize mealsByDate with each date having placeholders for each meal type
-  // let mealsByDate = datesForThisWeek.reduce((acc, date) => {
-  //   acc[date] = [{ type: 1 }, { type: 2 }, { type: 3 }]; // Assuming types 1, 2, 3 as placeholders
-  //   return acc;
-  // }, {});
-
-  // // Function to extract just the date part from the datetime string
-  // const extractDate = (datetime) => datetime.split("T")[0];
-
-  // // Populate the mealsByDate object with actual meals data
-  // meals.forEach((meal) => {
-  //   const mealDate = extractDate(meal.date);
-  //   if (mealsByDate.hasOwnProperty(mealDate)) {
-  //     // Check if the meal type already exists in the array for this date
-  //     const index = mealsByDate[mealDate].findIndex(
-  //       (placeHolder) => placeHolder.type === meal.type
-  //     );
-  //     if (index !== -1) {
-  //       // Replace the placeholder with the actual meal data
-  //       mealsByDate[mealDate][index] = {
-  //         meal_id: meal.meal_id,
-  //         type: meal.type,
-  //         recipe_id: meal.recipe_id,
-  //       };
-  //     } else {
-  //       // This case handles if you have more meal types than placeholders
-  //       mealsByDate[mealDate].push({
-  //         meal_id: meal.meal_id,
-  //         type: meal.type,
-  //         recipe_id: meal.recipe_id,
-  //       });
-  //     }
-  //   }
-  // });
-
-  // // Clean up: Remove placeholders for dates where meals are available but keep placeholders for empty dates
-  // Object.keys(mealsByDate).forEach((date) => {
-  //   if (mealsByDate[date].every((meal) => meal.hasOwnProperty("meal_id"))) {
-  //     // All meals for this date are actual meals, not placeholders
-  //     mealsByDate[date] = mealsByDate[date].filter((meal) =>
-  //       meal.hasOwnProperty("meal_id")
-  //     );
-  //   }
-  // });
 
   // console.log(mealsByDate);
 
@@ -81,9 +16,9 @@ function MealList2({ meals }) {
   // console.log(mealList);
   // console.log(meals);
   useEffect(() => {
-    // if (!!meals) {
-    setMealList(mealsDataFormater(meals));
-    // }
+    if (!!meals) {
+      setMealList(mealsDataFormater(meals));
+    }
   }, []);
 
   const handleDragAndDrop = (results) => {
@@ -91,6 +26,7 @@ function MealList2({ meals }) {
     // console.log(source);
     // console.log(destination);
     console.log(results);
+    console.log(mealList);
 
     // return when draggable is dropped outside of the droppable area
     if (!destination) return;
@@ -225,7 +161,7 @@ function MealList2({ meals }) {
         {/* Render meals grouped by date */}
         {Object.keys(mealList).map((date) => (
           <div key={date}>
-            <p>{date}</p>
+            <p>{date.split("T")[0]}</p>
             <ul>
               {mealList[date].map((meal) => (
                 <Droppable
@@ -236,28 +172,24 @@ function MealList2({ meals }) {
                   {(provided) => (
                     <div {...provided.droppableProps} ref={provided.innerRef}>
                       <li key={meal.meal_id}>
-                        {/* Render meal information */}
-                        {/* <div>{`Meal ID: ${meal.meal_id}`}</div> */}
-                        <div>{`Type: ${meal.type}`}</div>
-                        {/* <div>{`Recipe IDs: ${meal.recipe_id}`}</div> */}
-                        {meal.recipe_id &&
-                          meal.recipe_id.map((id, index) => (
-                            <Draggable
-                              draggableId={`${id}`}
-                              key={index}
-                              index={index}
-                            >
-                              {(provided) => (
-                                <div
-                                  {...provided.dragHandleProps}
-                                  {...provided.draggableProps}
-                                  ref={provided.innerRef}
-                                >
-                                  {id}
-                                </div>
-                              )}
-                            </Draggable>
-                          ))}
+                        <div>{meal.type}</div>
+                        {meal.recipes.map((recipe, index) => (
+                          <Draggable
+                            draggableId={`${recipe.recipe_uuid}`}
+                            key={recipe.recipe_uuid}
+                            index={index}
+                          >
+                            {(provided) => (
+                              <div
+                                {...provided.dragHandleProps}
+                                {...provided.draggableProps}
+                                ref={provided.innerRef}
+                              >
+                                {recipe.recipe_id}
+                              </div>
+                            )}
+                          </Draggable>
+                        ))}
                         {provided.placeholder}
                       </li>
                     </div>
